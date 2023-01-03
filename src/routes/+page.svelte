f097c0817fc70d4e10e8c858c8ae2a798162c8ae
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2023-01-03 10:39:01 -0500

    initial commit with some starter code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import "../app.css";
+</script>
+
+<img src="assets/obama.png" alt="Barack Obama" class="barack-obama-image"/>
+<div class="offset-right text-group">
+    <div style="--order: 1">
+        <h1>Why I Want to Work for the Obama Foundation</h1>
+    </div>
+    <div style="--order: 2">
+        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sit vitae ipsa ipsum tenetur facilis unde consequatur ipsam itaque nisi blanditiis nobis accusantium nostrum, vero error, incidunt ad cumque provident laboriosam rerum aliquam. Quo molestias soluta, exercitationem quam vitae sed a? Blanditiis asperiores fugiat sint unde alias mollitia beatae dolorum maiores animi? Dolore cum iste odit repudiandae illo excepturi perferendis pariatur incidunt, illum dignissimos repellat qui vitae exercitationem voluptatem libero voluptates, culpa, accusantium nihil ipsa corporis itaque sed numquam. Quisquam, tenetur expedita! Maiores ab in nobis quisquam dolore ipsam repudiandae enim, natus suscipit veniam praesentium consectetur excepturi autem iste dolor.</p>
+    </div>
+    <div style="--order: 3">
+        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sit vitae ipsa ipsum tenetur facilis unde consequatur ipsam itaque nisi blanditiis nobis accusantium nostrum, vero error, incidunt ad cumque provident laboriosam rerum aliquam. Quo molestias soluta, exercitationem quam vitae sed a? Blanditiis asperiores fugiat sint unde alias mollitia beatae dolorum maiores animi? Dolore cum iste odit repudiandae illo excepturi perferendis pariatur incidunt, illum dignissimos repellat qui vitae exercitationem voluptatem libero voluptates, culpa, accusantium nihil ipsa corporis itaque sed numquam. Quisquam, tenetur expedita! Maiores ab in nobis quisquam dolore ipsam repudiandae enim, natus suscipit veniam praesentium consectetur excepturi autem iste dolor.</p>
+    </div>
+    <div style="--order: 4">
+        <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat laudantium deserunt velit! Voluptatem, natus tempore quisquam quis aliquid similique inventore numquam nobis. Dicta sunt, autem consectetur obcaecati recusandae nisi ullam.</p>
+    </div>
+</div>
+
+
+<img src="assets/selector.png" alt="Selector bar for module section" class="selector-bar"/>
